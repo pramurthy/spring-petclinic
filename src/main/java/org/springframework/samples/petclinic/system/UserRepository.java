d213bb97f1de69406a76a4502075214dc263cf75
Ajit-Acharya-ML <ajit.acharya@maplelabs.com>
2023-06-05 11:06:44 +0530

    Added: Slow API

--- src/main/java/org/springframework/samples/petclinic/system/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/system/UserRepository.java
@@ -14,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query("from User where user_email=?1 and user_pass=?2")
 	public User findByUsernamePassword(String username, String password);
 
+	@Query(value = "select * ,sleep(80) from user where user_fname=?1", nativeQuery = true)
+	public User findByFname(String firstname);
+
 }
