856030d8623c01014b75332d17c5ef688600eaab
Pavethran-D-ML <135807462+Pavethran-D-ML@users.noreply.github.com>
2023-08-08 09:43:45 +0530

    Handling user duplicate entry

--- src/main/java/org/springframework/samples/petclinic/system/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/system/UserRepository.java
@@ -12,7 +12,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	public List<User> findByEMAIL(String email);
 
 	@Query("from User where user_email=?1 and user_pass=?2")
-	public User findByUsernamePassword(String username, String password);
+	public List<User> findByUsernamePassword(String username, String password);
 
 	@Query(value = "select * ,sleep(80) from user where user_fname=?1", nativeQuery = true)
 	public User findByFname(String firstname);
