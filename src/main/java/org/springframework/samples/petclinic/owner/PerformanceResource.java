b0beb5c16d83e419e1e118db92522ba9a843f123
Pavethran-D-ML <135807462+Pavethran-D-ML@users.noreply.github.com>
2023-08-22 12:11:30 +0530

    updated code

--- src/main/java/org/springframework/samples/petclinic/owner/PerformanceResource.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PerformanceResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
@@ -19,74 +20,74 @@ public class PerformanceResource {
 
 	private static final Logger logger = LoggerFactory.getLogger(PerformanceResource.class);
 
-	@GetMapping("/memoryleak")
-	public String createMemoryLeak(Model model) throws InterruptedException {
-	    logger.info("Memory Leak API is invoked");
-	    List<List<Users>> memoryLeakList = new ArrayList<>();
-	    model.addAttribute("msg", "This API will execute 10 mins");
-	    int numberOfLists = 0;
-	    long endTime = System.currentTimeMillis() + 10 * 60 * 1000; // 10 minutes
-
-	    while (System.currentTimeMillis() < endTime) {
-	        List<Users> usersList = new ArrayList<>();
-	        for (int i = 0; i < 100; i++) { // Create 100 objects in each inner list
-	            Users users = new Users();
-	            users.setAge(20);
-	            users.setId(1);
-	            users.setName("maplelabs");
-	            usersList.add(users);
-	        }
-	        memoryLeakList.add(usersList);
-	        numberOfLists++;
-	        
-	        if (numberOfLists % 10 == 0) { // Clear every 10 inner lists to maintain consistency
-	            memoryLeakList.subList(0, 5).clear();
-	        }
-	        
-	        Thread.sleep(1);
-	    }
-
-	    memoryLeakList.clear();
-	    logger.info("Total lists created: " + numberOfLists);
-	    logger.info("Memory Leak API is stopped");
-	    return "performance/performance";
-	}
+	List<Users> memoryLeakList = new ArrayList<>();
 
 	private volatile boolean stopFlag = false;
 
-	private List<Thread> cpuThreads = new ArrayList<>();
-
 	private ScheduledExecutorService executorService;
+
+	private Thread memroyLeak;
+
+	@GetMapping("/memoryleak")
+	public String createMemoryLeak(Model model) throws InterruptedException {
+		logger.info("Memory Leak API is invoked");
+		/*
+		 * long startTime = System.currentTimeMillis(); long targetDuration = 10 * 60 *
+		 * 1000; // 10 minutes in milliseconds long targetHeapSize = 100 * 1024 * 1024; //
+		 * 120MB in bytes
+		 */ // long initialHeapSize = Runtime.getRuntime().totalMemory();
+		memoryLeakList.clear();
+
+		List<Object> memoryLeakList = new ArrayList<>();
+		int i = 0;
+		Thread.currentThread().setName("Pet-MemoryLeak");
+		while (i <= 110) {
+			memoryLeakList.add(new byte[1024 * 1024]);
+			Thread.sleep(5);
+			i++;
+		}
+		Thread.sleep(600000);
+		memoryLeakList.clear();
+		System.gc();
+		return "performance/performance";
+	}
+
+	@GetMapping("/stopmemoryusage")
+	public String stopMemoryLoad() {
+		logger.info("Memory Load API is stopped");
+		stopFlag = true;
+
+		if (memroyLeak != null) {
+			logger.info("ArrayList object size::" + memoryLeakList.size());
+			memroyLeak.interrupt();
+		}
+
+		if (executorService != null) {
+			executorService.shutdownNow();
+			memoryLeakList.clear();
+			System.gc();
+			logger.info("ArrayList object size after clear::" + memoryLeakList.size());
+		}
+		return "performance/performance";
+	}
 
 	@GetMapping("/increasecpuusage")
 	public String increaseCpuUsage(Model model) {
-		List<Thread> cpuThreads = new ArrayList<>();
 		logger.info("CPU load API is invoked");
-		// model.addAttribute("cpu", "This API will excute 5 mins");
 		stopFlag = false;
-		double targetCpuUsage = 0.5; // Target CPU usage (50%)
-		int numberOfThreads = Runtime.getRuntime().availableProcessors(); // Number of
-																			// available
-																			// CPU cores
-		int targetThreads = (int) (numberOfThreads * targetCpuUsage); // Number of threads
-																		// to achieve //
-		// // target CPU
-		// usage
-		System.out.println(targetCpuUsage);
-		for (int i = 0; i < targetThreads; i++) {
-			logger.info("CPU load API is inside loop");
-			Thread thread = new Thread(() -> {
-				logger.info("CPU load API is inside thread");
-				Thread.currentThread().setName("CPU-Thread-Util");
+		for (int i = 0; i < 1; i++) {
+			Thread CPUIncentiveThread = new Thread(() -> {
+
 				while (!stopFlag) {
-					for (int j = 0; j < 1_000_000; j++) {
+					Thread.currentThread().setName("Pet-CPUIncentiveThread");
+					for (int j = 0; j < 200000; j++) {
+
 						Math.sqrt(Math.random());
 					}
 				}
 			});
+			CPUIncentiveThread.start();
 
-			cpuThreads.add(thread);
-			thread.start();
 		}
 
 		// Schedule a task to stop the execution after 5 minutes
@@ -98,47 +99,35 @@ public class PerformanceResource {
 
 	@GetMapping("/stopcpuusage")
 	public String stopCpuUsage() {
-		stopFlag = true;
-		// Wait for all CPU threads to finish
 		logger.info("CPU load API is stopped");
-		for (Thread thread : cpuThreads) {
-			try {
-				thread.join();
-			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		if (!cpuThreads.isEmpty() && executorService != null) {
-			// Clear the list of CPU threads
-			cpuThreads.clear();
+		stopFlag = true;
+		/*
+		 * // Wait for all CPU threads to finish for (Thread thread : cpuThreads) { try {
+		 * thread.join(); } catch (InterruptedException e) { e.printStackTrace(); } }
+		 */
+		// Clear the list of CPU threads
+		if (executorService != null) {
 			executorService.shutdownNow();
+			logger.info("cleared the memory");
 		}
 		return "performance/performance";
 	}
 
 	@Async
-
 	@GetMapping("/process")
-
 	public void processRequest() throws InterruptedException {
 
 		logger.info("Process request method is invoked");
 
 		synchronized (this) {
-
 			logger.info("inside the synchronization method");
-
-			Thread.currentThread().setName("Thread-blocked");
-
+			Thread.currentThread().setName("Pet-Thread-blocked");
 			Thread.sleep(10 * 60 * 1000); // Simulating 10 minutes of processing
 
 		}
 
 		// Process completed
-
 		logger.info("process request method is completed");
-
 	}
 
 	@GetMapping("/performance")
--- src/main/java/org/springframework/samples/petclinic/owner/UsersController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/UsersController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @Controller
 public class UsersController {
 
@@ -19,7 +21,7 @@ public class UsersController {
 	Logger logger = LoggerFactory.getLogger(UsersController.class);
 
 	@GetMapping("/userexception")
-	public String saveUsers(Model model) {
+	public String saveUsers(Model model, HttpServletResponse response) {
 		Users users = new Users(0, "Snappy", 23);
 		try {
 			usersRepository.save(users);
@@ -28,6 +30,7 @@ public class UsersController {
 		catch (Exception e) {
 			logger.error("Exception occurred due to:", e);
 			model.addAttribute("excp", "Duplicate entry 'Snappy' for key 'name'");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		}
 
 		return "performance/performance";
